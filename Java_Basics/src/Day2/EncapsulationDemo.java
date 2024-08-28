5bf4a2c6233136fdaccd818b8f5be8020821317f
S.KAVIN <kavinsarathy13579@gmail.com>
2024-08-28 10:11:39 +0530

    new commit

---
+++ Java_Basics/src/Day2/EncapsulationDemo.java
@@ -0,0 +1,8 @@
+package Day2;
+
+public class EncapsulationDemo {
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ Java_Basics/src/Day2/Program.java
@@ -0,0 +1,9 @@
+package Day2;
+
+public class Program {
+
+	public static void main(String[] args) {
+		System.out.println("SUMAN");
+	}
+
+}
